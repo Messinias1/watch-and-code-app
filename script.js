9b00e55046de3f91832ab44d5d6b4b89c6ce0e15
Carl Kakisis <carlkakisis.student@careerdevs.com>
2019-03-25 17:39:17 +0000

    V5 completed

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 var todoList = {
     todos: [],
     displayTodos: function() {
-        console.log('My Todos', this.todos);
+        if (this.todos.length === 0) {
+            console.log('Your todo list is empty!');
+        }
+        else {
+            console.log('My Todos:');
+            for (var i = 0; i < this.todos.length; i++) {
+                if (this.todos[i].completed === true) {
+                    console.log('(x)', this.todos[i].todoText);
+                }
+                else {
+                    console.log('( )', this.todos[i].todoText);
+                }
+            }
+        }
     },
     addTodo: function(todoText) {
-        this.todos.push({todoText: todoText, completed: false});
+        this.todos.push({ todoText: todoText, completed: false });
         this.displayTodos();
     },
     changeTodo: function(position, todoText) {
@@ -16,7 +29,7 @@ var todoList = {
         this.displayTodos();
     },
     toggleCompleted: function(position) {
-        var todo = this .todos[position];
+        var todo = this.todos[position];
         todo.completed = !todo.completed;
         this.displayTodos();
     }
