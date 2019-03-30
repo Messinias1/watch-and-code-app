d7680b0309a5691be9381607d55f38a6a6ff5f80
Carl Kakisis <carlkakisis.student@careerdevs.com>
2019-03-30 16:56:51 +0000

    version 2 completed

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-// Should have a place to store todos
 var todos = ['item 1', 'item 2', 'item 3'];
+// should have a function to display todos
+function displayTodos() {
+    console.log('My Todos:', todos);
+}
+displayTodos();
 
-// have a way to display todos
-console.log('My Todos:', todos);
-
-// have a way to add new todos
-todos.push('new item');
-
-//have a way to change a todo
-todos[0] = 'changed!';
-
-// have a way to delete a todo
-todos.splice(0, 1);
+// should have a function to add new todos
+function addTodo() {
+    todos.push('new todo');
+    displayTodos();
+}
+addTodo();
+//have a function to change a todo
+function changeTodo(position, newValue) {
+    todos[position] = newValue;
+    displayTodos();
+}
+changeTodo(0, 'changed');
+// have a function to delete a todo
+function deleteTodo(position) {
+    todos.splice(position, 1);
+    displayTodos();
+}
+deleteTodo(0, 1);
