2c47a61c2582d2af02ba7c22b0955fafa838e092
Carl Kakisis <carlkakisis.student@careerdevs.com>
2019-03-30 17:19:30 +0000

    version 4 completed

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
-// store our todos array on an object
-// have a display todos method
-// have an add todo method
-// have a change todo method
-// have a delete todo method
+
 var todoList = {
     todos: ['item 1', 'item 2', 'item 3'],
     displayTodos: function() {
         console.log('My Todos:', this.todos);
     },
-    addTodo: function(todo) {
-        this.todos.push(todo);
+    // todoList.addTodo should add objects
+    addTodo: function(todoText) {
+        this.todos.push({todoText: todoText, completed: false});
         this.displayTodos();
     },
-    changeTodo: function(position, newValue) {
-        this.todos[position] = newValue;
+    // todoList.changeTodo should change the todoText property
+    changeTodo: function(position, todoText) {
+        this.todos[position].todoText = todoText;
         this.displayTodos();
     },
     deleteTodo: function(position) {
         this.todos.splice(position, 1);
+        this.displayTodos();
+    },
+    // todoList.toggleCompleted should flip the completed property
+    toggleCompleted: function(position) {
+        var todo = this.todos[position];
+        todo.completed = !todo.completed;
         this.displayTodos();
     }
 };
