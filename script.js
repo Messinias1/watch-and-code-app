7e0e476f764b5ca1264d8a6c82660f569eccb738
Carl Kakisis <carlkakisis.student@careerdevs.com>
2019-03-30 17:57:46 +0000

    version 7 completed

--- script.js
+++ script.js
@@ -63,3 +63,16 @@ var todoList = {
         this.displayTodos();
     }
 };
+
+// clicking displayTodos should run todoList.displayTodos
+
+var displayTodosButton = document.getElementById('displayTodosButton');
+var toggleAllButton = document.getElementById('toggleAllButton');
+
+displayTodosButton.addEventListener('click', function(){
+    todoList.displayTodos();
+});
+
+toggleAllButton.addEventListener('click', function(){
+    todoList.toggleAll();
+});
