3aefd54f5e9370ee98f3e7bde72079bde7e247d4
Carl Kakisis <carlkakisis.student@careerdevs.com>
2019-04-05 15:48:05 +0000

    created new js file with dog names

---
+++ dogList.js
@@ -0,0 +1,14 @@
+// store dog Names
+var dogs = ['Caleb', 'Heidi', 'Napoleon', 'Snowbie'];
+
+// display dog names
+console.log('My Dogs:', dogs);
+
+// add new dogs
+dogs.push('Crystal');
+
+// change a dog name
+dogs[0] = 'Hansel';
+
+// delete a dog name
+dogs.splice(0, 1);
