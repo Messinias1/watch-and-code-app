b04030639997655c3ed43819172b1f9d081fb0ca
Carl Kakisis <carlkakisis.student@careerdevs.com>
2019-04-07 00:33:43 +0000

    deleted comments from previous lessons

--- dogList.js
+++ dogList.js
@@ -1,28 +1,3 @@
-// // store dog Names
-// var dogs = ['Caleb', 'Heidi', 'Napoleon', 'Snowbie'];
-
-
-// // function to display dog names
-// function displayDogs() {
-//     console.log('My Dogs:', dogs);
-// }
-// // function to add new dog names
-// function addDogName(dog) {
-//     dogs.push(dog);
-//     displayDogs();
-// }
-// // function to change a dog name
-// function changeDogName(position, newValue) {
-//     dogs[position] = newValue;
-//     displayDogs();
-// }
-// // function to delete a dog name
-// function deleteDogName(position) {
-//     dogs.splice(position, 1);
-//     displayDogs();
-// }
-
-// store dogNames on an object
 var dogNames = {
     dogs: [],
     // displayDogs method
